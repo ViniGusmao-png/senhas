02c4d246c1a2dc3568b5c3c8e6cd6c06126724eb
Vinicius <vinicius.gusmao.hxh@outlook.com>
2024-10-21 21:05:15 -0300

    adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useState } from "react";
+import style from './page.module.css'
+import { Button, CopyButton } from "@mantine/core";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [senha, setSenha] = useState('') //estado aonde eu vou armazenar o gerador de senha, utilizando o state, o torno mais dinamico e facil a rendirização
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+  const caracteres =[
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "1","2","3","4","5","6","7","8","9","0","!","@","#","$",
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+  ] // array aonde eu armazeno os caracteres que irei usar para fazer a senha 
+
+  const gerador = () => { //função criada para gerar uma senha atraves do array de cima e armazenar ela em setSenha
+    let novaSenha = '' // aqui eu fiz um let
+    for(let x = 0; x <=11;x++){
+      const contador = Math.floor(Math.random()*caracteres.length)
+      novaSenha += caracteres[contador]
+    }
+
+    setSenha(novaSenha)
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.main}>
+        <button className={style.button}
+          onClick={(e) =>{e.preventDefault(); gerador()}}
+        >Gerar senha</button>
+        <p className={style.text}>{senha}</p>
+        <CopyButton value={senha}>
+          {({ copied, copy }) => (
+            <Button className={style.buttonM} onClick={copy}>
+              {copied ? 'Copiado!' : 'Copiar senha'}
+            </Button>
+          )}
+        </CopyButton>
+      </div>
     </div>
   );
 }
